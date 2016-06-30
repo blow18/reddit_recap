142287af144c743cec1680349fd7dfd76806ec75
blow18 <blow18@punahou.edu>
2016-06-29 14:22:50 -1000

    Finished challenge

--- js/index.js
+++ js/index.js
@@ -33,11 +33,84 @@ window.onload = function () {
     if (req.readyState === XMLHttpRequest.DONE) {
       var data = JSON.parse(req.response)
       window.reddit_feed = data;
+
+      startProcess();
     }
   }
   var time = document.querySelectorAll('.time')
   for (var i = 0; i < time.length; i++) {
     var unixTime = time[i].innerHTML;
     time[i].innerHTML = moment.unix(unixTime);
+  }
+}
+
+// All custom code goes in here
+function startProcess() {
+  var feed = window.reddit_feed.data;
+  for (var i = 0; i < feed.length; i++) {
+    var article = feed[i];
+    console.log(article);
+
+    var column = document.createElement("div");
+    column.className = "col-sm-12 col-md-4";
+
+    var post = document.createElement("div");
+    post.className = "post";
+    column.appendChild(post);
+
+    var link = document.createElement("a");
+    link.href = "javascript:void(0)";
+    post.appendChild(link);
+
+    var thumbnail = document.createElement("div");
+    thumbnail.className = "thumbnail";
+    link.appendChild(thumbnail);
+
+    var image = document.createElement("img");
+    if (article.thumbnail.indexOf("http") != -1) {
+      image.src = article.thumbnail;
+    }
+    image.alt = "thumbnail";
+    thumbnail.appendChild(image);
+
+    var author = document.createElement("div");
+    author.className = "author";
+    author.innerHTML = article.author;
+    link.appendChild(author);
+
+    var title = document.createElement("div");
+    title.className = "title";
+    title.innerHTML = article.title;
+    link.appendChild(title);
+
+    var body = document.createElement("div");
+    body.className = "body";
+    if (article.body) {
+      body.innerHTML = article.body;
+    } else {
+      body.innerHTML = "No Body";
+    }
+    link.appendChild(body);
+
+    var time = document.createElement("div");
+    time.className = "time";
+    time.innerHTML = moment.unix(article.created);
+    link.appendChild(time);
+
+
+    document.getElementById("main").appendChild(column);
+
+
+    // Create element in memory
+    /*var container = document.createElement("div");
+    var title = document.createElement("div");
+
+    // Assign values to element
+    title.innerHTML = article.title;
+
+    // Nest elements inside each other
+    container.appendChild(title);
+
+    document.getElementById("main").appendChild(container);*/
   }
 }
